c291b8e95af702301a11abdf1bd15e1af5665cbe
Jin Araki <jinzam.rock@gmail.com>
2014-05-13 00:11:34 +0900

    Enable menu-bar-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,7 @@
             (normal-top-level-add-subdirs-to-load-path))))))
 (add-to-load-path "elisp" "conf" "public_repos")
 
-(menu-bar-mode 0)
+;(menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 
